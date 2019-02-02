27733297f18560786c50ac27598c037672305dfd
Karthik Raman <karthik.raman@gmail.com>
2019-02-02 21:00:07 +0530

    updates

--- bin/gen_ics.sh
+++ bin/gen_ics.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if [[ $# -lt 4 ]]
 then
-  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script]"
+  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script] [mini]"
   exit 1
 fi
-if [[ $# -eq 6 ]]
+if [[ $# -eq 7 ]]
+then
+  mini=$7  # Mini ICS without too many festivals
+  script=$6
+  y=$5
+elif [[ $# -eq 6 ]]
 then
   script=$6
   y=$5
@@ -12,9 +17,9 @@ elif [[ $# -eq 5 ]]
 then
   y=$5
   script="iast"
-elif [[ $# -gt 6 ]]
+elif [[ $# -gt 7 ]]
 then
-  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script]"
+  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script] [mini]"
   exit 1
 else
   y=`date +%Y`
@@ -29,7 +34,7 @@ scr=${script:0:4}
 
 echo -ne "Computing $y ICS calendar for panchangam for $city_name ($lat,$lon) - $tz in $script script... "
 cd ../panchangam
-python3 -m jyotisha.panchangam.scripts.ics $city_name $lat $lon $tz $y $script
+python3 -m jyotisha.panchangam.scripts.ics $city_name $lat $lon $tz $y $script $mini
 if [[ $? -eq 0 ]]
 then
   echo "done!"
