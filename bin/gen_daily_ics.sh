dd9d110c8a0709e4a53a0c5e651a7a527c793967
Karthik Raman <karthik.raman@gmail.com>
2019-05-17 22:25:39 +0530

    Create gen_daily_ics.sh

---
+++ bin/gen_daily_ics.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+if [[ $# -lt 4 ]]
+then
+  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script] [fmt] [lagna]"
+  exit 1
+fi
+if [[ $# -eq 5 ]]
+then
+  y=$5
+  script="devanagari"
+elif [[ $# -eq 6 ]]
+then
+  y=$5
+  script=$6
+elif [[ $# -eq 7 ]]
+then
+  y=$5
+  script=$6
+  fmt=$7
+elif [[ $# -eq 8 ]]
+then
+  y=$5
+  script=$6
+  fmt=$7
+  lagna="True"
+  lagnasuff="-lagna"
+elif [[ $# -gt 7 ]]
+then
+  echo "Usage: $0 <city name> <lat> <lon> <tz name> [year] [script] [fmt] [lagna]"
+  exit 1
+else
+  y=`date +%Y`
+  script="devanagari"
+fi
+
+city_name=$1
+lat=$2
+lon=$3
+tz=$4
+scr=${script:0:4}
+
+echo "Computing $y daily panchangam for $city_name ($lat,$lon) - $tz in $script script... "
+echo "***"
+cd ../panchangam/
+python3 -m jyotisha.panchangam.scripts.write_daily_panchangam_ics $city_name $lat $lon $tz $y $script $fmt $lagna 
+
+if [[ $? -eq 0 ]]
+then
+  echo "***"
+  echo "Completed panchangam computation successfully!"
+else
+  echo "error!"
+fi
