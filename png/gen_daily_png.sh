db6cc23cdaa5ff8ae3119f9fd12dc6f7abd36381
Karthik Raman <karthikraman@users.noreply.github.com>
2023-04-14 08:28:47 +0530

    daily img

--- png/gen_daily_png.sh
+++ png/gen_daily_png.sh
@@ -12,7 +12,8 @@ mkdir -p $name/daily
 k=1
 while [ $k -le $nPages ]
 do
-pdftoppm "$1" $name/daily/"`printf "%03d" $k`" -png -rx 300 -ry 300 -f $(( k+1 )) -singlefile
+## Change k+3 appropriately based on pages spend on navanayaka etc.
+pdftoppm "$1" $name/daily/"`printf "%03d" $k`" -png -rx 300 -ry 300 -f $(( k+3 )) -singlefile
 echo `date --date "$start_date +$((k-1)) days" "+%d %B %Y"`,`date --date "$start_date +$((k-2)) days" "+%d-%m-%y"`,20:00,`date --date "$start_date +$((k-2)) days" "+%d-%m-%y"`,20:30,https://github.com/karthikraman/panchangam/raw/master/png/$name/daily/`printf "%03d" $k`.png >> $name.csv
 k=$(( k+1 ))
 echo -ne .
